c25fc87ab280ec9086e95c18677157812980e5da
Jake Soenneker <jake@soenneker.com>
2025-05-09 12:18:43 -0500

    namespace

--- src/TelnyxClientUtil.cs
+++ src/TelnyxClientUtil.cs
@@ -3,19 +3,20 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.Extensions.Configuration;
 using Soenneker.Telnyx.Client.Abstract;
 using Soenneker.Utils.HttpClientCache.Abstract;
-using Soenneker.Utils.HttpClientCache.Dtos;
 
 namespace Soenneker.Telnyx.Client;
 
 /// <inheritdoc cref="ITelnyxClientUtil"/>
-public class TelnyxClientUtil : ITelnyxClientUtil
+public sealed class TelnyxClientUtil : ITelnyxClientUtil
 {
     private readonly IHttpClientCache _httpClientCache;
     private readonly IConfiguration _configuration;
 
+    private const string _clientId = nameof(TelnyxClientUtil);
     private const string _prodBaseUrl = "https://api.telnyx.com/v2/";
 
     public TelnyxClientUtil(IHttpClientCache httpClientCache, IConfiguration configuration)
@@ -26,31 +27,30 @@ public class TelnyxClientUtil : ITelnyxClientUtil
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
-        var token = _configuration.GetValueStrict<string>("Telnyx:Token");
-
-        var options = new HttpClientOptions
+        return _httpClientCache.Get(_clientId, () =>
         {
-            BaseAddress = _prodBaseUrl,
-            DefaultRequestHeaders = new System.Collections.Generic.Dictionary<string, string>
-            {
-                { "Authorization", $"Bearer {token}" }
-            }
-        };
+            var token = _configuration.GetValueStrict<string>("Telnyx:Token");
 
-        return _httpClientCache.Get(nameof(TelnyxClientUtil), options, cancellationToken);
+            return new HttpClientOptions
+            {
+                BaseAddress = _prodBaseUrl,
+                DefaultRequestHeaders = new System.Collections.Generic.Dictionary<string, string>
+                {
+                    { "Authorization", $"Bearer {token}" }
+                }
+            };
+        }, cancellationToken);
     }
 
     public void Dispose()
     {
         GC.SuppressFinalize(this);
-
-        _httpClientCache.RemoveSync(nameof(TelnyxClientUtil));
+        _httpClientCache.RemoveSync(_clientId);
     }
 
     public ValueTask DisposeAsync()
     {
         GC.SuppressFinalize(this);
-
-        return _httpClientCache.Remove(nameof(TelnyxClientUtil));
+        return _httpClientCache.Remove(_clientId);
     }
 }
