dbe2851ce3dbacba373f3bb61969bc6bade5e0a3
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:52 -0500

    xml commenting

--- src/Soenneker.Telnyx.Client/Abstract/ITelnyxHttpClient.cs
+++ src/Soenneker.Telnyx.Client/Abstract/ITelnyxHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Telnyx.Client.Abstract;
 /// </summary>
 public interface ITelnyxHttpClient : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
