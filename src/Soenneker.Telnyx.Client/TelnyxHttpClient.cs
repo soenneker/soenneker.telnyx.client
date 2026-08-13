cc7aeebd53f5baf23d13d045694e7bbdab455e24
Jake Soenneker <jake@soenneker.com>
2026-08-12 19:18:41 -0500

    better request and response logging

--- src/Soenneker.Telnyx.Client/TelnyxHttpClient.cs
+++ src/Soenneker.Telnyx.Client/TelnyxHttpClient.cs
@@ -3,8 +3,10 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.Extensions.Configuration;
+using Soenneker.HttpClients.LoggingHandler;
 using Soenneker.Telnyx.Client.Abstract;
 using Soenneker.Utils.HttpClientCache.Abstract;
 
@@ -15,26 +17,44 @@ public sealed class TelnyxHttpClient : ITelnyxHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
     private readonly IConfiguration _configuration;
+    private readonly ILogger<TelnyxHttpClient> _logger;
 
     private const string _clientId = nameof(TelnyxHttpClient);
     private static readonly Uri _prodBaseUrl = new("https://api.telnyx.com/v2/", UriKind.Absolute);
 
-    public TelnyxHttpClient(IHttpClientCache httpClientCache, IConfiguration configuration)
+    public TelnyxHttpClient(IHttpClientCache httpClientCache, IConfiguration configuration, ILogger<TelnyxHttpClient> logger)
     {
         _httpClientCache = httpClientCache;
         _configuration = configuration;
+        _logger = logger;
     }
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
         // No closure: state passed explicitly + static lambda
-        return _httpClientCache.Get(_clientId, (configuration: _configuration, prodBaseUrl: _prodBaseUrl), static state =>
+        return _httpClientCache.Get(_clientId, (configuration: _configuration, logger: _logger, prodBaseUrl: _prodBaseUrl), static state =>
         {
             var token = state.configuration.GetValueStrict<string>("Telnyx:Token");
+            bool logging = state.configuration.GetValue<bool>("Telnyx:RequestResponseLogging");
+
+            HttpMessageHandler? pipeline = null;
+
+            if (logging)
+            {
+                pipeline = new HttpClientLoggingHandler(state.logger, new HttpClientLoggingOptions
+                {
+                    LogLevel = LogLevel.Debug,
+                    RedactedHeaders = ["Authorization"]
+                })
+                {
+                    InnerHandler = new HttpClientHandler()
+                };
+            }
 
             return new HttpClientOptions
             {
                 BaseAddress = state.prodBaseUrl,
+                HttpMessageHandler = pipeline,
                 DefaultRequestHeaders = new System.Collections.Generic.Dictionary<string, string>
                 {
                     { "Authorization", $"Bearer {token}" }
